140905405efaf9eaff4e4772de1cf9766829e2c8
Dushyanth Ganesan <dushyanth.ganesan@gmail.com>
2020-12-04 07:59:50 -0500

    formatting

--- serial2txt.py
+++ serial2txt.py
@@ -4,14 +4,14 @@
 import serial
 from time import sleep
 
-serial = serial.Serial("COM18",115200,timeout = 1); # Open serial port at required baude rate
-f = open("output.txt","w");                         # open txt file to write
-print(serial);                                      # show serial port info on terminal
-#time.sleep(5);                                      # wait 5 sec
+serial = serial.Serial("COM18",115200,timeout = 1) # Open serial port at required baude rate
+f = open("output.txt","w")                         # open txt file to write
+print(serial)                                      # show serial port info on terminal
+#time.sleep(5)                                     # wait 5 sec (changes sampling frequency)
 
 # Serial readline loop
 while True:
-    data = serial.readline();
-    data = data.decode("utf-8"); # decode utf-8 unicode characters as str
-    print(data);                 # show on terminal
-    f.write(data);               # write to txt file 
+    data = serial.readline()
+    data = data.decode("utf-8")  # decode utf-8 unicode characters as str
+    print(data)                  # show on terminal
+    f.write(data)                # write to txt file 
